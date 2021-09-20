72d8172c8a9056ff37deb7c606e119681422fa32
Gabriel Dudlicek <g.dudlicek@gmail.com>
2021-09-20 10:09:37 -0400

    wrote most of board class, next need to implement setPiece correctly

--- Project2/board.cpp
+++ Project2/board.cpp
@@ -4,61 +4,80 @@ using namespace std;
 #include <string.h>
 
 class Board {
-    char board[8][8];
-    int* No8(int x, int y);
+    char board[64];
+    int directions[8][2] = {{-1,0},{-1,-1},{-1,1},{1,0},{1,-1},{1,1},{0,-1},{0,1}};
+
     public:
         Board(char ourColor);
-        int* getLegalMoves();
+        char getPiece(int row, int col);
+        bool setPiece(int row, int col, char color);
         string boardToStr();
+        int getPieceNumFromCoords(int row, int col);
 };
+
+bool outOfBounds(int row, int col){
+    return (row < 0) || (row > 7) || (col < 0) || (col > 7);
+}
 
 Board::Board(char ourColor) {
     memset(board, 0, sizeof(board));
 
-    // DOUBLE CHECK COLORS are CORRECT
-    if (ourColor == 'b'){ // we go first
-        board[3][3]='b';
-        board[4][3]='o';
-        board[3][4]='o';
-        board[4][4]='b';
-    } else { // we go second
-        board[3][3]='o';
-        board[4][3]='b';
-        board[3][4]='b';
-        board[4][4]='o';
-    }
-    // now we're always blue (I think)
+        for (int i = 0; i < sizeof(board); i++) {
+            board[i] = '0';
+        }   
+
+        board[27]= 'o';
+        board[28]= 'b';
+        board[35]= 'b';   
+        board[36]= 'o';
 }
 
-// neighbors fcn
-int* Board::No8(int x, int y){
-    // implement getting neighbors
-    //
+char Board::getPiece(int row, int col){
+    //get the piece from the coordinates passed in
+    return board[row * 8 + col];
 }
 
-// get neighbor in a direction
-
-int* Board::getLegalMoves() {
-    // so copy the referee
-
-    // iterate through each spot, looking for empty
-    for (int i=0; i<8; i++){
-        for (int j=0; j<8; j++){
-            // if empty, look for 1 or more oranges in a row
-                // then look for a blue at the end --> return that place as a legal move
-        }
-    }
-    // if yes return
+int Board::getPieceNumFromCoords(int row, int col){
+    return row * 8 + col;
 }
 
+bool Board::setPiece(int row, int col, char color){
+    
+//     if (!outOfBounds(row, col) && !getPiece(row, col)=='0' ){
+//         for(int[] dir : directions){
+//             int rowCurr = row;
+//             int colCurr = col;
+//             bool potentialFlipped[64];
+//             bool legalMove = false;
+//             while (!outOfBounds(rowCurr, colCurr))
+//             {
+//                 rowCurr += dir[0];
+//                 colCurr += dir[1];
+//             }
+//             char colorCurr = getPiece(rowCurr, colCurr);
+//             if (colorCurr = '0'){
+//                 break;
+//             }else if (colorCurr = color)
+//             {
+//                 for (bool potentialFlip : potentialFlipped){
+//                     if (potentialFlip){
+//                         legalMove = true;
+//                     }
+//                 }
+//             }
+            
+//         }
+//     }
 
+//     board[row * 8 + col] = color;
+    return false;
+}
 
 string Board::boardToStr(){
     string output("");
     for (int i=0; i<8; i++){
         for (int j=0; j<8; j++){
-            if (board[i][j] == 0) output+=" ";
-            else output += board[i][j];
+            output += getPiece(i, j);
         }
         output += '\n';
     }
@@ -68,6 +87,7 @@ string Board::boardToStr(){
 
 int main() {
     Board b = Board('b');
+    
     cout << b.boardToStr() << endl;
 
     return 0;
