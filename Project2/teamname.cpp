9a59e43b1cbf8ac8189a236d9b009ae9bae21040
Tim McCarthy <timothy.mcc@comcast.net>
2021-09-22 17:13:10 -0400

    fixed reading last line fcn

--- Project2/teamname.cpp
+++ Project2/teamname.cpp
@@ -21,28 +21,17 @@ inline bool goFileExists() {
 	return (stat((teamname + ".go").c_str(), &buffer) == 0);
 }
 
-// from https://stackoverflow.com/questions/11876290/c-fastest-way-to-read-only-last-line-of-text-file
-string readLastLine(fstream* fin) {
-	(*fin).seekg(-1, ios_base::end);           // go to one spot before the EOF
 
-	bool keepLooping = true;
-	while (keepLooping) {
-		char ch;
-		(*fin).get(ch);                        // Get current byte's data
+string readLastMove(fstream* fin) {
+	string lastLine, prevLine;
 
-		if ((int) (*fin).tellg() <= 1) { 		// If the data was at or before the 0th byte
-			(*fin).seekg(0);                   // The first line is the last line
-			keepLooping = false;            // So stop there
-		} else if (ch == '\n') {            // If the data was a newline
-			keepLooping = false;            // Stop at the current position.
-		} else {          					// If the data was neither a newline nor at the 0 byte
-			(*fin).seekg(-2, ios_base::cur); 	// Move to the front of that data, then to the front of the data before it
-		}
+	// stops reading before empty line
+	// maybe need to refactor if other players mess it up
+	while (getline(*fin, lastLine) && lastLine != ""){
+		prevLine = lastLine;
 	}
 
-	string lastLine;
-	getline(*fin, lastLine);
-	return lastLine;
+	return prevLine;
 }
 
 int main() {
@@ -54,16 +43,23 @@ int main() {
 
 	while (playing) {
 		while (!goFileExists()) {
-			// cout << "waiting" << endl;
-		} // spin while waiting
-
+			// spin while waiting
+		}
+		playing = false;
+		cout << "go file found; opening move_file" << endl;
 		// open move_file and read last line
 		move_file.open("move_file");
-		lastMove = readLastLine(&move_file);
+
+		lastMove = readLastMove(&move_file);
+
+		cout << "last move was: " << lastMove << endl;
 
 		// if we made the last move, continue
-		if (lastMove.find(teamname, 0)) continue; // UNTESTED lol
+		if (lastMove.find(teamname, 0)){
+			cout << "last move us" << endl;
+			continue; // UNTESTED lol
 
+		}
 		// remove opponent name
 		opponentMove = lastMove.substr(lastMove.find(" ") + 1, lastMove.size()); // UNTESTED
 
