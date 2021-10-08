80e32a91ca2ee1c4256f7ecd7a85b559603c114f
Tim <timothy.mcc@comcast.net>
2021-10-08 16:28:36 -0400

    started preprocessing data

--- Project3/Project3.py
+++ Project3/Project3.py
@@ -1,9 +1,36 @@
+import numpy.random
 from keras.models import Sequential
 from keras.layers import Dense, Activation
 import numpy as np
 
-# Model Template
+# pre-process data
+images = np.load('data/images.npy')
+labels = np.load('data/labels.npy')
 
+print(images[0].shape)
+
+num_samples = 6500
+num_classes = 10
+samples_per_class = int(num_samples/num_classes)
+
+x_train, x_val, x_test = [], [], []
+y_train, y_val, y_test = [], [], []
+
+for i in range(num_classes):
+    img_class = images[samples_per_class*i:(samples_per_class*(i+1))]
+    numpy.random.shuffle(img_class)
+    x_train.extend(img_class[:390])  # first 60% is training
+    x_val.extend(img_class[390:488])  # next 15% is validation
+    x_test.extend(img_class[488:])  # last 25% is test
+
+    one_hot_v = np.zeros(10)
+    one_hot_v[i] = 1
+    y_train.extend([one_hot_v, ] * 390)
+    y_val.extend([one_hot_v, ] * 98)
+    y_test.extend([one_hot_v, ] * 162)
+
+
+# Model Template
 model = Sequential() # declare model
 model.add(Dense(10, input_shape=(28*28, ), kernel_initializer='he_normal')) # first layer
 model.add(Activation('relu'))
@@ -23,8 +50,9 @@ model.compile(optimizer='sgd',
               metrics=['accuracy'])
 
 # Train Model
+print(type(x_train[0]), type(y_train[0]))
 history = model.fit(x_train, y_train, 
-                    validation_data = (x_val, y_val), 
+                    validation_data=(x_val, y_val),
                     epochs=10, 
                     batch_size=512)
 
