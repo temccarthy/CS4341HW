596ec9fecd4f473aa6af791fb4022bc3db83e1c0
Gabriel Dudlicek <g.dudlicek@gmail.com>
2021-10-11 02:45:53 -0400

    fixed error with placing illegal move in board class, also added default pick to a legal move if the timer runs out

---
+++ Project2/RefFiles/board.hpp
@@ -0,0 +1,53 @@
+/*
+ * board.hpp
+ *
+ *  Created on: Sep 21, 2021
+ *      Author: tim
+ */
+
+#ifndef BOARD_HPP_
+#define BOARD_HPP_
+
+using namespace std;
+
+#include <string.h>
+#include <iostream>
+#include <vector>
+
+class Board {
+	char board[64]; // the board array
+public:
+	// default to we go first
+	char ourColor = 'b'; // which color we are
+	char opponentColor = 'o'; // opponent color
+
+
+	Board();
+	Board(Board* board);
+
+	Board* copyBoard();
+
+	void setOurColor(char color);
+
+	char getPiece(int row, int col);
+	int getPieceNumFromCoords(int row, int col);
+
+	bool setPiece(int row, int col, char color);
+	bool setPiece(char row, int col, char color);
+	bool setPiece(int boardPos, char color);
+
+	vector<int> flippedPieces(int row, int col, char color);
+
+	bool isGameOver();
+	float utility();
+	float evaluate();
+	int mobility(char color);
+	int numStableDiscs(char color);
+	int numCorners(char color);
+	int numFrontierDiscs(char color);
+
+	void testCases();
+	string boardToStr();
+};
+
+#endif /* BOARD_HPP_ */
---
+++ Project2/RefFiles/minimax.hpp
@@ -0,0 +1,82 @@
+/*
+ * minimax.hpp
+ *
+ *  Created on: Sep 25, 2021
+ *      Author: tim
+ */
+
+#ifndef MINIMAX_HPP_
+#define MINIMAX_HPP_
+
+using namespace std;
+#include "board.hpp"
+#include "Timer.cpp"
+
+struct UtilityMovePair {
+	int move;
+	float utility;
+	UtilityMovePair(int m, float u) {
+		move = m;
+		utility = u;
+	}
+
+	bool equals(UtilityMovePair* newPair){
+		return (move == newPair->move && utility == newPair->utility);
+	}
+	
+};
+
+class Minimax {
+	bool hitITL;
+
+	// order to check moves in, instead of just checking 1, 2, 3, 4 etc
+	// it goes in a spiral from the middle out
+	int moveOrder[64] = {27, 28, 36, 35, 34, 26, 18, 19,
+					  20, 21, 29, 37, 45, 44, 43, 42,
+					  41, 33, 25, 17, 9, 10, 11, 12,
+					  13, 14, 22, 30, 38, 46, 54, 53,
+					  52, 51, 50, 49, 48, 40, 32, 24,
+					  16, 8, 0, 1, 2, 3, 4, 5,
+					  6, 7, 15, 23, 31, 39, 47, 55,
+					  63, 62, 61, 60, 59, 58, 57, 56};
+	char ourColor,opponentColor;
+public:
+	int timeLimit;
+	bool timeUp = false;
+
+	Minimax(int timeLimit);
+	int minimaxSearch(Board* board, int ITL, Timer* t);
+private:
+	UtilityMovePair* maxValue(Board* board, int ply, int ITL, float alpha, float beta, Timer* t);
+	UtilityMovePair* minValue(Board* board, int ply, int ITL, float alpha, float beta, Timer* t);
+};
+
+#endif /* MINIMAX_HPP_ */
+
+
+/*
+Python code for generating moveOrder
+
+a = 27
+l = []
+multiplier = 1
+lambdas = [lambda x: x+(1),
+lambda x: x+(8),
+lambda x: x-(1),
+lambda x: x-(8)]
+lambda_ctr = 0
+
+l.append(a)
+while True:
+    for i in range(multiplier):
+        a = lambdas[lambda_ctr](a)
+        l.append(a)
+    lambda_ctr = (lambda_ctr + 1) % 4
+    if lambda_ctr % 2 == 0:
+        multiplier += 1
+
+    if len(l) >= 64:
+        break
+
+print(l[:64])
+ */
